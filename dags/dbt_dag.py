61f57db1dfdfbc8427886e332ac202615cacfe2f
Priyanka Srivastava <psrivastava@phdata.io>
2025-05-25 01:34:16 +0530

    correct dbt_dag

--- dags/dbt_dag.py
+++ dags/dbt_dag.py
@@ -19,7 +19,7 @@ dbt_snowflake_dag = DbtDag(
     operator_args={"install_deps": True},
     profile_config=profile_config,
     execution_config=ExecutionConfig(
-        dbt_executable_path=f"{os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt"
+        dbt_executable_path=f"{os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt",
     ),
     schedule=None,
     start_date=datetime.now() + timedelta(minutes=1),
