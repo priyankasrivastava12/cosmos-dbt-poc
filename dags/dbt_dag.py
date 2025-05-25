d1ddccf0345569e629317572dc0498b49657e34d
Priyanka Srivastava <psrivastava@phdata.io>
2025-05-25 13:23:10 +0530

    fix dbt dag -iter four

--- dags/dbt_dag.py
+++ dags/dbt_dag.py
@@ -22,4 +22,4 @@ dbt_snowflake_dag = DbtDag(project_config=ProjectConfig("/usr/local/airflow/dags
                     schedule="@daily",
                     start_date=datetime(2023, 9, 10),
                     catchup=False,
-                    dag_id="dbt_dag",)
+                    dag_id="cosmos_test_dag",)
