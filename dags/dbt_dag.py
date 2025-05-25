a3cc39fe1c595e723c52c96b9d7177e86eb1082a
Priyanka Srivastava <psrivastava@phdata.io>
2025-05-25 12:48:45 +0530

    fix dbt dag -iter two

--- dags/dbt_dag.py
+++ dags/dbt_dag.py
@@ -1,6 +1,5 @@
 import os
 from datetime import datetime, timedelta
-
 from cosmos import DbtDag, ProjectConfig, ProfileConfig, ExecutionConfig
 from cosmos.profiles import SnowflakeUserPasswordProfileMapping
 
@@ -15,13 +14,12 @@ profile_config = ProfileConfig(profile_name="dbtlearn",
                                                         },
                                                     ))
 
-dbt_snowflake_dag = DbtDag(
-    project_config=ProjectConfig("/usr/local/airflow/dags/dbt/dbtlearn"),
-    operator_args={"install_deps": True},
-    profile_config=profile_config,
-    execution_config=ExecutionConfig(dbt_executable_path=f"{os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt",),
-    schedule=None,
-    start_date=datetime.now() + timedelta(minutes=1),
-    catchup=False,
-    dag_id="dbt_dag",
-)
+
+dbt_snowflake_dag = DbtDag(project_config=ProjectConfig("/usr/local/airflow/dags/dbt/dbtlearn",),
+                    operator_args={"install_deps": True},
+                    profile_config=profile_config,
+                    execution_config=ExecutionConfig(dbt_executable_path=f"{os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt",),
+                    schedule=None,
+                    start_date=datetime.now() + timedelta(minutes=1),
+                    catchup=False,
+                    dag_id="dbt_dag",)
